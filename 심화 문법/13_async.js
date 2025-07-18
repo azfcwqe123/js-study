cb37d25cee7a43dce8ccfc03a4e2951e8be721cb
azfcwqe123 <azfcwqe123@gmail.com>
2025-07-18 13:31:26 +0900

    async

--- 심화 문법/13_async.js
+++ 심화 문법/13_async.js
@@ -2,9 +2,12 @@ const setTimeoutPromise = (ms) => new Promise((resolve, reject) => {
     setTimeout(resolve, ms);
 });
 
-const main = async() => { // 이렇게도 가능
-    await setTimeoutPromise(1000);
-    console.log("1초 뒤에 실행됩니다.");
+async function main() {
+    await setTimeoutPromise(2000);
+    console.log("2초 뒤에 실행됩니다.");
+    console.log("내가 나중에");
 }
 
 main();
+
+// await은 async 함수에서 사용할 수 있음. function 앞에 async가 붙지 않으면 작동하지 않음.
