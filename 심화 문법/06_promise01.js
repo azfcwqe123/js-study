b9f60dce592f30f7db3d35a764f2abe1ef9b66fa
azfcwqe123 <azfcwqe123@gmail.com>
2025-07-18 15:40:20 +0900

    프로미스

--- 심화 문법/06_promise01.js
+++ 심화 문법/06_promise01.js
@@ -1,6 +1,9 @@
 // new promise : 비동기 작업을 감싸서 나중에 성공(resolve)하거나 실패(reject)로 결과를 알려주는 약속(Promise)을 만드는 것.
+// Promise는 "나중에 결과를 알려줄게!"라는 약속 객체
+// 동기냐 비동기냐는 중요하지 않고, "결과가 있으면 resolve(), 문제가 있으면 reject()를 호출"하면 되는 구조
+// resolve, reject 각각의 이름은 정해진건 아님. 바꿀 수 있음.
 
-const setTimeoutPromise = new Promise((resolve, reject) => { // setTimeoutPromise는 프로미스 객체가 됨.
+const setTimeoutPromise = new Promise((resolve, reject) => { // setTimeoutPromise는 Promise 객체가 됨.
     setTimeout(() => { // setTimeout(resolve, 0); 이와 동일
         resolve(); // () => resolve()는 resolve 함수를 감싼 익명함수일뿐, 동작은 같음
     }, 0);
@@ -20,22 +23,5 @@ setTimeoutPromise2(3000).then(() => {
     console.log("3초 뒤에 실행됩니다.");
 }); 
 
-// 예제1
 
-const setTimeoutPromise3 = (ms) => new Promise((resolve, reject) => {
-    setTimeout(resolve, ms);
-});
 
-const promise3 = setTimeoutPromise3(0);
-
-console.log("다른 일을 하다가");
-console.log("필요할 때");
-console.log("then을 호출해보세요.");
-
-promise3.then(() => {
-    console.log("0초 뒤 실행");
-});
-
-promise3.catch((err) => {
-    console.log("에러 발생시 실행됩니다.");
-})
