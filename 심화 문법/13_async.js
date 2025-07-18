f331eed4b5d6f9a5586479a93f12fae4f4464e30
azfcwqe123 <azfcwqe123@gmail.com>
2025-07-18 13:15:23 +0900

    async

--- 심화 문법/13_async.js
+++ 심화 문법/13_async.js
@@ -2,7 +2,7 @@ const setTimeoutPromise = (ms) => new Promise((resolve, reject) => {
     setTimeout(resolve, ms);
 });
 
-const main = async() => {
+const main = async() => { // 이렇게도 가능
     await setTimeoutPromise(1000);
     console.log("1초 뒤에 실행됩니다.");
 }
